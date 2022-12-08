e366dc2ab0dc0a192e64b0cb16a6d787b3374972
Filip Samól <114443811+Z3iTHx@users.noreply.github.com>
2022-12-08 14:16:16 +0100

    Add files via upload

--- MyPhotoblog.pl/JS/general.js
+++ MyPhotoblog.pl/JS/general.js
@@ -1,13 +1,10 @@
 const bg = document.querySelector(".header")
 
-const img = document.querySelector("#bg")
-
 window.addEventListener("mousemove", function(e){
-    
-
-    let x = `calc(50% + ${e.clientX / 90}px)`
+    let x = `calc(30% + ${e.clientX / 90}px)`
     let y = `calc(80% + ${e.clientY / 90}px)`
 
     bg.style.backgroundPositionX = x
     bg.style.backgroundPositionY = y
 })
+
